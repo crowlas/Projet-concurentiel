a9fb2196976331f7f587ca98ff78a94c046fd2f4
crowlas <dracosamu@gmail.com>
2017-10-20 15:13:27 +0200

    Exercice 1 terminé. Installation de l'exercice 2.

--- prog2/concur/exercice1/Philosophe.java
+++ prog2/concur/exercice1/Philosophe.java
@@ -24,6 +24,10 @@ class Baguette
     _prise = false;
     notifyAll();
   }
+  
+  public boolean get_prise(){
+	  return this._prise;
+  }
 }
 
 public class Philosophe implements Runnable
@@ -40,30 +44,58 @@ public class Philosophe implements Runnable
 
   public void run()
   {
-    while( true )
+	
+	int nbTour = 10;
+	int tour=0;
+	int cptPenser = 0;
+	int cptManger = 0;
+	
+    recommencer : while(tour<nbTour )
     {
+    tour++;
       penser();
+      cptPenser++;
       _bGauche.prendre();
+      if (_bDroite.get_prise()){
+    	  _bGauche.relacher();
+    	  continue recommencer;
+      }
       _bDroite.prendre();
       manger();
+      cptManger++;
       _bDroite.relacher();
       _bGauche.relacher();
     }
+	
+	System.out.println(this._nom+" a pensé "+ cptPenser+" fois et à mangé "+cptManger+" fois en "+nbTour+" tours d'actions.");
   }
 
   final void manger() 
   {
-    System.out.println( _nom + " mange." );
+    //System.out.println( _nom + " mange." );
+    try {
+		Thread.sleep(60);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
   }
 
   final void penser() 
   {
-    System.out.println( _nom + " pense." );
+    //System.out.println( _nom + " pense." );
+    try {
+		Thread.sleep(60);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
   }
 
   public static void main( String args[] )
   {
-    final String[] noms = { "Platon", "Socrate", "Aristote", "Diogène", "Sénèque" };
+	  System.out.println("*********************NOUVEAU TEST*****************");
+	final String[] noms = { "Platon", "Socrate", "Aristote", "Diogène", "Sénèque" };
     final Baguette[] baguettes = { new Baguette(), new Baguette(), new Baguette(), new Baguette(), new Baguette() }; 
     Philosophe[] table;
 
